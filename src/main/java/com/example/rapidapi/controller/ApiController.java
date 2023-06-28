dc97bb973ef5937b83bc7ee7e17973bcb48bd0d1
MUHSIN MOHAMMED KP <muhsinmhdkp@gmail.com>
2023-06-28 11:03:41 +0530

    second commit

--- src/main/java/com/example/rapidapi/controller/ApiController.java
+++ src/main/java/com/example/rapidapi/controller/ApiController.java
@@ -21,4 +21,9 @@ public class ApiController {
     public String getAlluArjunImages(@PathVariable String query) {
         return rapidApiService.makeRapidApiRequest(query);
     }
+
+    @GetMapping("/welcome")
+    public String getWelcome(){
+        return "Welcome to api";
+    }
 }
